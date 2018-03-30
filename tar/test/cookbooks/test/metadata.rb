608a674838dc10b999ce7ed7021c518248429284
Bryan Chua <me@bryanchua.com>
2018-03-31 00:17:27 +0800

    Add tar receipe

---
+++ tar/test/cookbooks/test/metadata.rb
@@ -0,0 +1,9 @@
+name 'test'
+maintainer 'Community Cookbook Team'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache-2.0'
+description 'Installs/Configures test'
+version '0.1.0'
+
+depends 'tar'
+depends 'build-essential'
---
+++ tar/test/cookbooks/test/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook:: test
+# Recipe:: default
+#
+# Copyright:: 2016-2017, Chef Software, Inc.
+
+apt_update 'update'
+
+include_recipe 'tar::default'
+include_recipe 'build-essential::default'
+
+package 'ncurses-devel' if platform_family?('suse') # needed for nano compile
+
+tar_package 'https://www.nano-editor.org/dist/v2.8/nano-2.8.7.tar.gz' do
+  prefix '/usr/local'
+  creates '/usr/local/bin/nano'
+end
+
+tar_extract 'https://www.nano-editor.org/dist/v2.8/nano-2.8.7.tar.gz' do
+  target_dir '/usr/local/nano_tar_extract'
+  creates '/usr/local/nano_tar_extract/configure'
+  tar_flags ['-P', '--strip-components 1']
+end
