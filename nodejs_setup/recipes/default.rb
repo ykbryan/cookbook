b73252c39fa55d5f51804c0b9b65f948f0390006
Bryan Chua <me@bryanchua.com>
2018-03-31 01:09:32 +0800

    Update receipe

--- nodejs_setup/recipes/default.rb
+++ nodejs_setup/recipes/default.rb
@@ -5,15 +5,24 @@ package "git" do
   options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
 end
 
+package 'unzip' do
+    action :install
+end
+
 application app_path do
   javascript "4"
   environment.update("PORT" => "80")
   environment.update(app["environment"])
 
-  tar_extract "#{app["app_source"]["url"]}" do
-    target_dir app["app_path"]
-    tar_flags [ '--strip-components 1' ]
-    action :extract
+  remote_file app["app_source"]["url"] do
+    source '/tmp/code.zip'
+    mode '0755'
+    action :create
+  end
+
+  bash 'extract_code_zip' do
+    cwd app["app_path"]
+    code 'unzip /tmp/code.zip'
   end
 
   # if app['type'] == 's3'
