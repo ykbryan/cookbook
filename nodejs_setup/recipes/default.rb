bc9e8f7d59773b2310e04cc2dc0985e246e85ce2
Bryan Chua <me@bryanchua.com>
2018-03-31 09:33:55 +0800

    Debugging

--- nodejs_setup/recipes/default.rb
+++ nodejs_setup/recipes/default.rb
@@ -17,6 +17,14 @@ end
 s3_remote_path = uri_path_components.join("/")
 s3_base_uri.to_s.chomp!("/")
 
+# package "git" do
+#   # workaround for:
+#   # WARNING: The following packages cannot be authenticated!
+#   # liberror-perl
+#   # STDERR: E: There are problems and -y was used without --force-yes
+#   options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
+# end
+
 application app_path do
   javascript "4"
   environment.update("PORT" => "80")
@@ -47,9 +55,9 @@ application app_path do
     action :run
   end
 
-  # zipfile "#{tmpdir}/archive" do
-  #   into "#{app_path}"
-  #   overwrite true
+  # git app_path do
+  #   repository app["app_source"]["url"]
+  #   revision app["app_source"]["revision"]
   # end
 
   link "#{app_path}/server.js" do
