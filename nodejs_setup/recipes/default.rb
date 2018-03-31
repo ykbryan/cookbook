db09b0551afa3db2996b3d4c176bc62f6d600f78
Bryan Chua <me@bryanchua.com>
2018-03-31 12:06:39 +0800

    DEBUGGING

--- nodejs_setup/recipes/default.rb
+++ nodejs_setup/recipes/default.rb
@@ -17,49 +17,48 @@ end
 s3_remote_path = uri_path_components.join("/")
 s3_base_uri.to_s.chomp!("/")
 
-package "git" do
-  # workaround for:
-  # WARNING: The following packages cannot be authenticated!
-  # liberror-perl
-  # STDERR: E: There are problems and -y was used without --force-yes
-  options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
-end
+# package "git" do
+#   # workaround for:
+#   # WARNING: The following packages cannot be authenticated!
+#   # liberror-perl
+#   # STDERR: E: There are problems and -y was used without --force-yes
+#   options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
+# end
 
 application app_path do
   javascript "4"
   environment.update("PORT" => "80")
   environment.update(app["environment"])
 
-  # tmpdir = Dir.mktmpdir("opsworks")
-  # directory tmpdir do
-  #   owner 'ec2-user'
-  #   group 'ec2-user'
-  #   mode 0755
-  # end
-  #
-  # aws_s3_file "#{tmpdir}/archive" do
-  #   bucket s3_bucket
-  #   remote_path s3_remote_path
-  #   retries 3
-  # end
-  #
-  # file "#{tmpdir}/archive" do
-  #   mode '0755'
-  # end
-  #
-  # execute 'extract_code' do
-  #   cwd "#{app_path}"
-  #   user 'root'
-  #   retries 3
-  #   # command "cd #{app_path} && unzip -o #{tmpdir}/archive"
-  #   command "unzip -o #{tmpdir}/archive"
-  #   action :run
-  # end
-
-  git app_path do
-    repository app["app_source"]["url"]
-    revision app["app_source"]["revision"]
+  tmpdir = Dir.mktmpdir("opsworks")
+  directory tmpdir do
+    owner 'ec2-user'
+    group 'ec2-user'
+    mode 0755
   end
+
+  s3_file "#{tmpdir}/archive" do
+    bucket s3_bucket
+    remote_path s3_remote_path
+  end
+
+  file "#{tmpdir}/archive" do
+    mode '0755'
+  end
+
+  execute 'extract_code' do
+    cwd "#{app_path}"
+    user 'root'
+    retries 3
+    # command "cd #{app_path} && unzip -o #{tmpdir}/archive"
+    command "unzip -o #{tmpdir}/archive"
+    action :run
+  end
+
+  # git app_path do
+  #   repository app["app_source"]["url"]
+  #   revision app["app_source"]["revision"]
+  # end
 
   link "#{app_path}/server.js" do
     to "#{app_path}/index.js"
